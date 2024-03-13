af4f217c01b5259009ac80d9a46fcbd40a38b308
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2024-03-13 12:53:11 +0530

    pan card authentication

--- upload.php
+++ upload.php
@@ -2,6 +2,7 @@
 
 $vehiclenumber = $_POST['vehicleNumber'];
 $product_id = $_POST['vehicleType'];
+$model = $_POST['model'];
 $description = $_POST['description'];
 $price = $_POST['price'];
 $startdate = $_POST['startdate'];
@@ -11,7 +12,7 @@ include_once "connection.php";
 
 session_start();
 $owner_id = $_SESSION['owner_id'];
-$status = mysqli_query($conn, "insert into vehicle (owner_id, vehicle_num, product_id, description, price, startdate, enddate) values ('$owner_id', '$vehiclenumber', '$product_id', '$description', '$price', '$startdate', '$enddate')");
+$status = mysqli_query($conn, "insert into vehicle (owner_id, vehicle_num, product_id, model,  description, price, startdate, enddate) values ('$owner_id', '$vehiclenumber', '$product_id', '$model', '$description', '$price', '$startdate', '$enddate')");
 
 if($status){
     echo "Regsitration Successful";
